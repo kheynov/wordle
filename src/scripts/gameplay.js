d698785c6cf70fd108084eba8aab7c281a7e9124
riuga <mosks@bk.ru>
2022-07-28 19:43:48 +0700

    feat: add check for a word

--- src/scripts/gameplay.js
+++ src/scripts/gameplay.js
@@ -1,24 +1,27 @@
-const keys = document.getElementsByClassName("key");
-
 export default function playGame(lang) {
+	const keys = document.getElementsByClassName("key");
 	const attempts = [[]];
+	const state = [];
 	let row = 0;
-	console.log(lang);
-	// let word;
-	// lang === "ru" ? (word = "удило") : (word = "aboba");
+	let data;
+	getData(lang)
+		.then((response) => (data = response))
+		.catch((err) => console.error(err));
 
+	// listening key press
 	for (let i = 0; i < keys.length; i++) {
 		keys[i].onclick = ({ target }) => {
 			const letter = target.getAttribute("data-key");
 			if (row < 6) {
 				if (letter === ">>") {
 					if (attempts[row].length === 5) {
+						state.push(check(attempts[row], data.word.split("")));
 						row++;
 						attempts.push([]);
+						console.log(state);
 					}
 				} else if (letter === "backspace") {
 					attempts[row].pop();
-					console.log(attempts[0]);
 					draw(attempts[row], row);
 				} else if (attempts[row].length < 5) {
 					attempts[row].push(letter);
@@ -41,4 +44,44 @@ function draw(word, row) {
 		child = wordsOnBoard[row].children[i];
 		child.innerHTML = filledWord[i];
 	}
+}
+
+async function getData(lang) {
+	const options = { method: "POST" };
+	const response = await fetch(
+		`https://wordle.kheynov.ru/api/word/get?lang=${lang}`,
+		options
+	);
+	const data = await response.json();
+	return await data;
+}
+
+function check(guess, correct) {
+	const attempt = guess.slice();
+	const word = correct.slice();
+	const state = ["", "", "", "", ""];
+	for (let i = 0; i < 5; i++) {
+		if (attempt[i] === word[i]) {
+			state[i] = "X";
+			attempt[i] = "";
+			word[i] = "";
+		}
+	}
+
+	for (let i = 0; i < 5; i++) {
+		let index = word.findIndex(char => char === attempt[i]);
+		if (attempt[i] !== "" && index !== -1) {
+			word[index] = "";
+			attempt[i] = "";
+			state[i] = "C";
+		}
+	}
+
+	state.forEach((el) => {
+		if (el === "") {
+			el = "M";
+		}
+	});
+
+	return state;
 }
